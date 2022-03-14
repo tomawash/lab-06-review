fbae01d7eb4524994c41ae3afca6d7cc4e581dd9
Tomasz Wiercioch <47215529+tomawash@users.noreply.github.com>
2022-03-14 17:00:21 -0400

    added remaining assignments

--- bmi.py
+++ bmi.py
@@ -2,4 +2,4 @@ import sys
 weight = float(input("Please enter weight (in pounds) "))
 height = float(input("Please enter height (in inches) "))
 bmi = round((703 * weight) / pow(height, 2))
-print("Your body mann index (BMI) is: " + str(bmi))
+print("Your body mass index (BMI) is: " + str(bmi))
